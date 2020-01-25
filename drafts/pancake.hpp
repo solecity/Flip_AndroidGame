01fd628d3d306b9220158f1ae91ade83d0178a2d
solecity <solecity@hotmail.com>
2020-01-25 23:07:55 +0100

    initial drafts

---
+++ drafts/pancake.hpp
@@ -0,0 +1,13 @@
+
+
+class Pancake : public Food
+{
+	
+	static const int number_of_ascending_keyframes = 3;
+	static const int number_of_falling_keyframes = 3;
+	
+public:
+
+	Pancake();
+
+};
---
+++ drafts/strawberry.hpp
@@ -0,0 +1,13 @@
+
+
+class Strawberry : public Food
+{
+	
+	static const int number_of_ascending_keyframes = 1;
+	static const int number_of_falling_keyframes = 1;
+	
+public:
+
+	Strawberry();
+
+};
