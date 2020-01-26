6a0a4dfe460063041c7c4fb52d0a6c8d68ab2d41
solecity <solecity@hotmail.com>
2020-01-26 17:51:25 +0000

    apk

--- code/main.cpp
+++ code/main.cpp
@@ -37,9 +37,12 @@ int main ()
 // falta provocando errores. Dejar estas referencias aquí por el momento para evitar esos problemas:
 void keep_links ()
 {
-    const bool &c = Window::can_be_instantiated;
+    ( void )Window :: can_be_instantiated;
     Window::Accessor window;
+    Window::create_window (0);
+    Window::get_window (0);
     Graphics_Resource_Cache cache;
     opengles::Context::create(window, &cache);
     Canvas::Factory f = opengles::Canvas_ES2::create;
+    Texture_2D::register_factory (0 ,0 );
 }
